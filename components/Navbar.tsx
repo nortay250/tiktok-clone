60b8a6c23f6c894564cbd62a06c4622edceacc27
nortay250 <eleanortay97@gmail.com>
2022-07-08 09:42:12 +0800

    added main video view and google auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -21,6 +23,17 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>Search</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log('error')}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
 import Discover from './Discover';
@@ -34,30 +33,7 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className='px-2 py-4 hidden xl:block'>
-              <p className='text-gray-400'>
-                Log in to like and comment on videos
-              </p>
-              <div className='pr-4'>
-                <GoogleLogin
-                  clientId=''
-                  render={(renderProps) => (
-                    <button
-                      className='bg-white cursor-pointer text-lg text-[#f51997] border-[1px] border-[#f51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#f51997]'
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Log in
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy={'single_host_origin'}
-                />
-              </div>
-            </div>
-          )}
+
           <Discover />
           <SuggestedAccounts />
           <Footer />
