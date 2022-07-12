90dca67d8933e841af13a036f658a021574d8410
nortay250 <eleanortay97@gmail.com>
2022-07-12 23:29:54 +0800

    add video details

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,6 +8,7 @@ import { MdDelete } from 'react-icons/md';
 
 import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
+import { BASE_URL } from '../utils';
 
 const Upload = () => {
   const router = useRouter();
@@ -68,7 +69,7 @@ const Upload = () => {
         topic: category,
       };
       console.log(document);
-      await axios.post('http://localhost:3000/api/post', document);
+      await axios.post(`${BASE_URL}/api/post`, document);
       router.push('/');
     }
   };
